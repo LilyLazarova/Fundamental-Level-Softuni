aaefe2c559621df47ad82bdf9c990d5dc13f57cf
LilyLazarova <lility@gmail.com>
2016-03-14 00:01:15 +0200

    adding OOP folder

---
+++ OOP/PlayWithClasses/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+    class Dog
+    {
+        public Dog() :this(null,null)
+        {
+            
+        }
+
+        public Dog(string name,string breed)
+        {
+            this.Name = name;
+            this.Breed = breed;
+        }
+            public string Name { get; set; }
+            public string Breed { get; set; }
+
+            public void Bark()
+            {
+                Console.WriteLine("{0} ({1}) says BAU",this.Name ?? "[unnamed dog]",this.Breed);
+            }
+
+
+    }
+
